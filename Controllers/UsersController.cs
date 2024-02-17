9391d5391660feb8d6bc9d38b20c185a4645c2eb
Ianina Voda <yaninavoda@gmail.com>
2024-02-17 12:46:26 +0200

    complete project

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using ProductsValidation.Models;
 using ProductsValidation.Services;
-using System.Data;
 
 namespace ProductsValidation.Controllers
 {
