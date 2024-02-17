2cbea8dceb7e7fbf400b8b008827ac3355ab6608
Ianina Voda <yaninavoda@gmail.com>
2024-02-17 12:35:47 +0200

    complete project

--- Services/Data.cs
+++ Services/Data.cs
@@ -5,14 +5,14 @@ namespace ProductsValidation.Services
 {
     public class Data
     {
-        public List<Product> Products = new List<Product>
+        public List<Product> Products = new()
         {
-            new Product() {Id = 1, Name = "Product2", Description = "ProductDescription"},
+            new Product() {Id = 1, Name = "Product2", Description = "ProductDescription", Type = Product.Category.Technique},
             new Product() {Id = 2, Name = "Product3", Description = "ProductDescription"},
-            new Product() {Id = 3, Name = "Product4", Description = "ProductDescription"},
+            new Product() {Id = 3, Name = "Product4", Description = "ProductDescription", Type = Product.Category.Clothes},
             new Product() {Id = 4, Name = "Product5", Description = "ProductDescription"},
-            new Product() {Id = 5, Name = "Product6", Description = "ProductDescription"},
-            new Product() {Id = 6, Name = "Product1", Description = "ProductDescription"},
+            new Product() {Id = 5, Name = "Product6", Description = "ProductDescription", Type = Product.Category.Transport},
+            new Product() {Id = 6, Name = "Product1", Description = "ProductDescription", Type = Product.Category.Transport}
         };
 
         public List<User> Users = new List<User>
@@ -21,7 +21,8 @@ namespace ProductsValidation.Services
             new User() {Id = 2, Name = "Guest", Email = "guest@gmail.com", Role = "guest"},
             new User() {Id = 3, Name = "User", Email = "user1@gmail.com", Role = "user"},
             new User() {Id = 4, Name = "User2", Email = "user2@gmail.com", Role = "user"},
-
         };
+
+        
     }
 }
